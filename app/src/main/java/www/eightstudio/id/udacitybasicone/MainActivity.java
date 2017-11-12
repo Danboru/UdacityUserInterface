31c29ac96be42cb4ccf90de640985d6bef6fbc8b
Danboru <8danang8@gmail.com>
2017-11-12 14:38:04 +0700

    First Init

--- app/src/main/java/www/eightstudio/id/udacitybasicone/MainActivity.java
+++ app/src/main/java/www/eightstudio/id/udacitybasicone/MainActivity.java
@@ -2,42 +2,16 @@ package www.eightstudio.id.udacitybasicone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-
-
-    Button button;
-    RadioGroup radioGroup;
-    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-         button =  findViewById(R.id.btnTampilData);
-         radioGroup = findViewById(R.id.radioButtonGroupSatu);
-
-         button.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-
-                 int selectedID = radioGroup.getCheckedRadioButtonId();
-                 radioButton = findViewById(selectedID);
-                 Integer data = 10;
-                 Number dataKU = data;
-
-                 Toast.makeText(MainActivity.this, "" + radioButton.getText() + " " + dataKU, Toast.LENGTH_SHORT).show();
-
-             }
-         });
-
     }
+
 }
